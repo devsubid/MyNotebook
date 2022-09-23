021cdc893f6072c1cb2eb2306afd6b1482ab90c4
SUBID DAS <itsmesubid@gmail.com>
2022-09-24 00:23:46 +0530

    Add `credits`

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -45,7 +45,7 @@ function About() {
           <div className="svg"></div>Open Source:
         </h3>
         <p>
-          MyNotebook is an open source project. You can find the source code on
+          MyNotebook is an open source project. You can find the source code on{" "}
           <a
             href="https://github.com/itsme-Subid/MyNotebook"
             target={"_blank"}
@@ -55,6 +55,40 @@ function About() {
           </a>
           <br />
           I'm currently using <strong>react.js</strong> to build MyNotebook.
+        </p>
+      </div>
+      <div className="credits">
+        <h3>
+          <div className="svg"></div>Credits:
+        </h3>
+        <p>
+          MyNotebook is built by{" "}
+          <a
+            href="https://github.com/itsme-Subid"
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            Subid
+          </a>
+          .<br />
+          The icons used in MyNotebook are from{" "}
+          <a
+            href="https://ionic.io/ionicons"
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            Ionicons
+          </a>
+          .<br />
+          The font used in MyNotebook is from{" "}
+          <a
+            href="https://fonts.google.com/"
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            Google Fonts
+          </a>
+          .
         </p>
       </div>
     </div>
